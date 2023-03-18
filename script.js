68a185f648f374ed05181455b70eae3ab0e07745
Pongsapak598 <pongsapak.roc@gmail.com>
2023-03-19 01:43:29 +0700

    Final version without GUI - Changed answer's input box to buttons

--- script.js
+++ script.js
@@ -4,53 +4,51 @@ let comAnswer;
 function comStart() {
     num = Math.floor(Math.random() * 3) + 1;
     if (num === 1) {
-        comAnswer = 'rock';
+        comAnswer = 'Rock';
     } else if (num === 2) {
-        comAnswer = 'paper';
+        comAnswer = 'Paper';
     } else if (num === 3) {
-        comAnswer = 'scissors';
+        comAnswer = 'Scissors';
     } else console.log('comStart Error');
 }
 
 let userValue;
 
-function getValue() {
-    let container = document.getElementById("box").value;
-    userValue = container.toLowerCase();
+function getValue(value) {
+    userValue = value;
 }
 
 let result;
 
 function process() {
-    if (userValue === 'rock') {
-        if (comAnswer === 'rock') {
+    if (userValue === 'Rock') {
+        if (comAnswer === 'Rock') {
             result ='Draw!';
-        } else if (comAnswer === 'paper') {
+        } else if (comAnswer === 'Paper') {
             result ='You lose!';
-        } else if (comAnswer === 'scissors') {
+        } else if (comAnswer === 'Scissors') {
             result ='You win!';
         } else alert ('You must type Rock, Paper or Scissors!');
 
-    } else if (userValue === 'paper') {
-        if (comAnswer === 'rock') {
+    } else if (userValue === 'Paper') {
+        if (comAnswer === 'Rock') {
             result ='You win!';
-        } else if (comAnswer === 'paper') {
+        } else if (comAnswer === 'Paper') {
             result ='Draw!';
-        } else if (comAnswer === 'scissors') {
+        } else if (comAnswer === 'Scissors') {
             result ='You lose!';
         } else alert ('You must type Rock, Paper or Scissors!');
 
-    } else if (userValue === 'scissors') {
-        if (comAnswer === 'rock') {
+    } else if (userValue === 'Scissors') {
+        if (comAnswer === 'Rock') {
             result ='You lose!';
-        } else if (comAnswer === 'paper') {
+        } else if (comAnswer === 'Paper') {
             result ='You win!';
-        } else if (comAnswer === 'scissors') {
+        } else if (comAnswer === 'Scissors') {
             result ='Draw!';
         } else alert ('You must type Rock, Paper or Scissors!');
 
     } else {
-        result ="Your answer doesn't seem right..."
         comAnswer = '';
     }
 }
@@ -63,11 +61,11 @@ if (typeof result === 'string') {
 
 let playerPoint = 0;
 let comPoint = 0;
-let Button = document.getElementById('summitButton');
+let buttons = document.querySelectorAll('#value-button');
 
-function whoWin() {
+function gameoverText() {
     if (playerPoint === 5) {
-        document.getElementById("show-dead").innerHTML = "You're survived!";
+        document.getElementById("show-dead").innerHTML = "You've survived!";
         freezeGame();
     } else if (comPoint === 5) {
         document.getElementById("show-dead").innerHTML = "You're dead!";
@@ -78,21 +76,23 @@ function whoWin() {
 }
 
 function freezeGame() {
-    Button.disabled = true;
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
 }
 
 function countPoint() {
     if (result === 'You win!') {
         playerPoint ++;
         document.getElementById("show-point").innerHTML = `Point<br />Player: ${playerPoint}<br />Com: ${comPoint}`;
-        whoWin();
+        gameoverText();
     } else if (result === 'You lose!') {
         comPoint ++;
         document.getElementById("show-point").innerHTML = `Point<br />Player: ${playerPoint}<br />Com: ${comPoint}`;
-        whoWin();
+        gameoverText();
     } else {
         document.getElementById("show-point").innerHTML = `Point<br />Player: ${playerPoint}<br />Com: ${comPoint}`;
-        whoWin();
+        gameoverText();
     }
 }
 
@@ -103,9 +103,10 @@ function reset() {
     result = '';
     playerPoint = 0;
     comPoint = 0;
-    document.getElementById('box').value = '';
     document.getElementById("show-answer").innerHTML = '';
     document.getElementById("show-point").innerHTML = '';
     document.getElementById("show-dead").innerHTML = "";
-    Button.disabled = false;
+    buttons.forEach(button => {
+        button.disabled = false;
+    });
 }
